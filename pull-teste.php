1b8352b6006059fbf9cfa95d260e3393c7fb8a23
jonatas-doc <jon_contigo@hotmail.com>
2015-11-23 11:52:31 -0200

    INICIANDO PROJETO NA VERAAA

--- pull-teste.php
+++
@@ -1,2 +0,0 @@
-<?php
-//arquivo que o branch 2 deve fazer o pull
--- teste-doc.php
+++
@@ -1,8 +0,0 @@
-<?php
-//testandooooooo
-
-//apaguei a segunda linha no master e escrevi isso;
-
-//cada um pode fazer sua edição e manter sua áre, tem que sempre fazer o pull ou o merge
-
-//aqui eu adicionei com um branck jonatas-doc
